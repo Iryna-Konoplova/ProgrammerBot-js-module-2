161ba8b9b084e6f47f8546c76a15fc0b52056695
Iryna Konoplova <a.irina773@gmail.com>
2021-02-23 01:46:54 +0200

    Добавляет вариант решения задачи 25

--- js/task-25.js
+++ js/task-25.js
@@ -43,6 +43,20 @@ function getCommonElements(array1, array2) {
 }
 
 
+// Еще один вариант решения
+
+// function getCommonElements(array1, array2) {
+// //     // Пиши код ниже этой строки
+//     const newArray = [];
+// for (let i = 0; i < array1.length; i += 1) {
+// if (array2.includes(array1[i])) {
+//           newArray.push(array1[i]); 
+//        } 
+
+// }
+//       return newArray;
+// }
+
   console.log(getCommonElements([1, 2, 3], [2, 4]));
   console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
   console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
